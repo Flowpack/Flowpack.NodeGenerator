42463d495bffbdada21d3cd07fa89e727c3ea71c
Dominique Feyer <dfeyer@ttree.ch>
2014-03-01 21:44:26 +0100

    [TASK] Improve Image and TextWithImages node generator
    - Add support for caption

--- Classes/Flowpack/NodeGenerator/Generator/Content/ImageGeneratorImplementation.php
+++ Classes/Flowpack/NodeGenerator/Generator/Content/ImageGeneratorImplementation.php
@@ -6,6 +6,7 @@ namespace Flowpack\NodeGenerator\Generator\Content;
  *                                                                        *
  *                                                                        */
 
+use TYPO3\Faker\Lorem;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
 use TYPO3\TYPO3CR\Domain\Model\NodeType;
@@ -23,6 +24,13 @@ class ImageGeneratorImplementation extends TextGeneratorImplementation {
 	public function create(NodeInterface $parentNode, NodeType $nodeType) {
 		$node = parent::create($parentNode, $nodeType);
 		$node->setProperty('image', $this->getRandommImageVariant());
+		
+		if (rand(0,10) === 0) {
+			$node->setProperty('hasCaption', TRUE);
+			$node->setProperty('caption', Lorem::sentence(rand(5, 12)));
+			$node->setProperty('title', Lorem::sentence(rand(5, 12)));
+			$node->setProperty('alternativeText', Lorem::sentence(rand(5, 12)));
+		}
 
 		return $node;
 	}
